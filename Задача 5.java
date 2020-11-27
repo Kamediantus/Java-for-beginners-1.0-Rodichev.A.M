fba3a4aca66a4dbdb5ef778ecc1cd79f05cdd34e
Kamediantus <62592194+Kamediantus@users.noreply.github.com>
2020-11-27 11:48:58 +0300

    Задача №5_ Операторы Родичев А.М.

---
+++ Задача 5.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Ввести 2 числа с клавиатуры, затем один из символов ‘+’, ‘-’, ‘*’ или ‘/’.
+        double num1, num2, result;
+        String sign;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Please enter first number: ");
+        num1 = in.nextDouble();
+        System.out.print("Please enter second number: ");
+        num2 = in.nextDouble();
+        System.out.print("Please enter sign: ");
+        sign = in.next();
+        System.out.println();
+//        В зависимости от символа вывести в консоль результат выполнения соответствующей арифметической операции.
+        switch (sign){
+            case "+": result = num1 + num2;
+                System.out.println(result);
+                break;
+                case "-": result = num1 - num2;
+                    System.out.println(result);
+                    break;
+            case "*": result = num1 * num2;
+                System.out.println(result);
+                break;
+            case "/": result = num1 / num2;
+                System.out.println(result);
+                break;
+            default:
+                System.out.println("Please enter correct sign:");
+        }
+
+    }
+}
+
