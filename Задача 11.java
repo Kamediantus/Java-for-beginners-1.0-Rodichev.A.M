3d907e03d9e514309b9b5d3b3ca6faaa630c0710
Kamediantus <62592194+Kamediantus@users.noreply.github.com>
2020-11-27 14:24:43 +0300

    Задача №11_Работа с данными Родичев А.М.

---
+++ Задача 11.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String num1;
+        int num2;
+
+//                Ввести первое  число с клавиатуры и записать его в строковую переменную.
+        System.out.print("Введите 1 число: ");
+        num1 = in.nextLine();
+//                Ввести второе число с клавиатуры и сохранить его в целочисленную переменную типа int.
+        System.out.print("Введите 2 число: ");
+        num2 = in.nextInt();
+//                Сравнить 2 числа и вывести большее на экран .
+//                Выведите также меньшее число на экран, предварительно переконвертировать его в тип double.
+        if (Integer.parseInt(num1) < num2){
+            System.out.printf("Число %d - большее.\n", num2);
+            System.out.printf("Число %.2f - меньшее", (double)Integer.parseInt(num1));
+        }
+        else {
+            System.out.printf("Число %d - большее.\n", Integer.parseInt(num1));
+            System.out.printf("Число %.2f - меньшее", (double)num2);
+        }
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
