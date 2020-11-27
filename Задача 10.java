6383b5d9cccbde146ad31b5b618b938daad701ec
Kamediantus <62592194+Kamediantus@users.noreply.github.com>
2020-11-27 14:15:01 +0300

    Задача №10_Циклы Родичев А.М.

---
+++ Задача 10.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int lines;
+        int columns;
+//        Напишите программу, где пользователь вводит данные с клавиатуры в матрицу,
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите размерность матрицы. Количество строк: ");
+        lines = in.nextInt();
+        System.out.print("Количество столбцов: ");
+        columns = in.nextInt();
+        double [][] nums = new double[lines][columns];
+
+        for (int i = 0; i < lines; i++){
+            for (int j = 0; j < columns; j++){
+                System.out.printf("Координаты элемента: [%d][%d]. Введите значение элемента:", i, j );
+                nums[i][j] = in.nextDouble();
+            }
+        }
+        //                а после этого произведите вывод первой строки матрицы на экран,
+        //                где каждый элемент умножается на 3. Размерность матрицы задается пользователем.
+        for (int i = 0; i < lines; i++){
+            System.out.printf("Элемент [%d][0] * 3 = %.2f\n", i, nums[i][0]*3);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
