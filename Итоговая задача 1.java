dd7a7c58b907c2ebf0bcc91a882c55bd9e854654
Kamediantus <62592194+Kamediantus@users.noreply.github.com>
2020-11-27 16:11:18 +0300

    Итоговая задача №1 Родичев А.М.

---
+++ Итоговая задача 1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Напишите программу конвертер валют. Программа должна переводить рубли в доллары по текущему курсу.
+//        Пользователь вводит текущий курс и количество рублей.
+//        Итоговое значение должно быть округлено до двух знаков после запятой.
+        Scanner in = new Scanner(System.in);
+        double dollarRate;
+        double rubles;
+
+        System.out.print("Введите текущий курс доллара к рублю: ");
+        dollarRate = in.nextDouble();
+        if (dollarRate > 35){
+        System.out.println("Оу... Где мой 2013?(((");}
+        System.out.print("Введите количество рублей: ");
+        rubles = in.nextDouble();
+        System.out.printf("%.2f руб. в долларах = %.2f$ при курсе %.2f", rubles, rubles/dollarRate, dollarRate);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
