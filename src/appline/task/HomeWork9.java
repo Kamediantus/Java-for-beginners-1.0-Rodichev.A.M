912611ae48d8d96643970eb42bf4ad5be4abbce9
Kamediantus <liprikent@yandex.ru>
2020-12-02 13:41:34 +0500

    Add task 9

---
+++ src/appline/task/HomeWork9.java
@@ -0,0 +1,26 @@
+package src.appline.task;
+
+import java.util.Scanner;
+
+public class HomeWork9 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double [] nums;
+        double avg = 0;
+//            Пользователь вводит размер массива и данные с клавиатуры в массив типа double.
+        System.out.print("Введите размер массива:");
+        nums = new double[in.nextInt()];
+        System.out.println("Введите элементы массива, после каждого элемента нажмите enter:");
+        for (int i = 0; i < nums.length; i++){
+            nums[i] = in.nextDouble();
+            avg += nums[i];
+        }
+//            Посчитайте среднее арифметическое элементов массива.
+        avg /= nums.length;
+        System.out.println("avg: " + avg);
+//            После этого произведите вывод массива на экран, где каждый элемент массива умножается на среднее арифметическое
+        for (int i = 0; i< nums.length; i++){
+            System.out.println(nums[i] * avg);
+        }
+    }
+}
