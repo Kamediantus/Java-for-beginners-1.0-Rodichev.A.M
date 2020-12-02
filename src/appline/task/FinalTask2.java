aff37ceb58abe6b1e520af1e4c8503dd6c63fb2e
Kamediantus <liprikent@yandex.ru>
2020-12-02 13:46:42 +0500

    Add FinalTask 2

---
+++ src/appline/task/FinalTask2.java
@@ -0,0 +1,43 @@
+package src.appline.task;
+
+import java.util.Scanner;
+
+public class FinalTask2 {
+    public static void main(String[] args) {
+//        Напишите программу, которая позволит решить простое уравнение относительно x.
+        Scanner in = new Scanner(System.in);
+        String str;
+        int a, b, result;
+        int sign;
+
+        System.out.print("Введите ваше уравнение: ");
+        str = in.nextLine();
+        if (str.substring(1, 2).equals("+")) {
+            sign = 1;
+        } else sign = 2;
+        int indexOfX = str.indexOf("x");
+        if (indexOfX == 0){
+            a = Integer.parseInt(str.substring(2,3));
+            b = Integer.parseInt(str.substring(4,5));
+            if (sign == 1) {
+                result = b - a;
+            } else result = b + a;
+        }
+        else if (indexOfX == 2) {
+            a = Integer.parseInt(str.substring(0, 1));
+            b = Integer.parseInt(str.substring(4, 5));
+            if (sign == 1) {
+                result = b - a;
+            } else result = a - b;
+        }
+        else {
+            a = Integer.parseInt(str.substring(0, 1));
+            b = Integer.parseInt(str.substring(2, 3));
+            if (sign == 1) {
+                result = a + b;
+            } else result = a - b;
+        }
+        System.out.printf("x = " + result);
+    }
+}
+
